cdcc586c45f12d6f597ba1e5f156305a5108b80f
janaavi <34087661+janaavi@users.noreply.github.com>
2022-12-31 10:45:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ gunicorn=20.1.0
 from flask import Flask, request, render_template, redirect
 from werkzeug.utils import secure_filename
 import os 
+import tensorflow as tf
 from tensorflow.keras.utils import load_img, img_to_array
-from tensorflow.keras.applications.mobilenet import MobileNet
-from tensorflow.keras.applications.mobilenet import decode_predictions
-from tensorflow.keras.applications.mobilenet import preprocess_input
 import numpy as np
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
-classifier = MobileNet(weights='imagenet')
+classifier = load_model('tl_covid_model_93.h5')
+class_names = np.array(['COVID', 'NORMAL', 'PNEUMONIA'])
 app.config['UPLOAD_FOLDER'] = './static/'
 
 @app.route('/')
@@ -42,16 +42,21 @@ def predict():
 	ff = secure_filename(janafile)
 	full_filename = os.path.join(app.config['UPLOAD_FOLDER'],ff)
 	
-	myimage    = load_img(full_filename, target_size=(224, 224))
+	myimage    = load_img(full_filename, target_size=(160, 160))
 	test_image = img_to_array(myimage)
-	
 	test_image = np.expand_dims(test_image, axis = 0)
-	test_image = preprocess_input(test_image)
-	preds      = classifier.predict(test_image,verbose=0)
-	result     = decode_predictions(preds, top=1)[0]
-		
-	return render_template('predicted.html', prediction_text='Prediction: '+result[0][1], prediction_percent='Probability: '+str(round(result[0][2]*100,2))+'%', user_image = ff)
+	result     = classifier.predict(test_image,verbose=0)
+	
+	predicted_id = tf.math.argmax(result, axis=-1)
+	predicted_class_label = class_names[predicted_id]
+	
+	pr_result = tf.math.softmax(result[0])
+	prediction = predicted_class_label
+	prediction_proba = pr_result[predicted_id[0]].numpy()
+	
+	return render_template('predicted.html', prediction_text='Prediction: '+prediction, prediction_percent='Probability: '+str(round(prediction_proba*100,2))+'%', user_image = ff)
 
+	
 @app.route('/ok', methods = ['GET', 'POST'])
 def okk():
 	return render_template('index.html')
@@ -61,5 +66,5 @@ def gohome():
 	return redirect('https://scholarlabfoundation.tech/')
 
 if __name__ == "__main__":
-	app.run(debug=False,threaded=False)
-    #app.run(debug=True)
+	#app.run(debug=False,threaded=False)
+    app.run(debug=True)
